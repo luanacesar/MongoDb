3262cd9026704660395857b3a282199e6e73f4e8
luanacesar <luana_cesar@hotmail.com>
2021-02-09 16:36:09 -0500

    first commit

---
+++ FruitsProject/app.js
@@ -0,0 +1,82 @@
+//jshit esversion:6
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'fruitsDB';
+
+const client = new MongoClient(url, { useNewUrlParser: true});
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+    insertDocuments(db, function() {
+    client.close();
+    });
+});
+
+const insertDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('fruirs');
+    // Insert some documents
+    collection.insertMany([
+        {
+        name:"Apple",
+        score:5,
+        review:"IT is an ok FRUIT"
+        },
+
+        {
+        name:"Orange",
+        score:8,
+        review:"GREAT FRUIT"
+        },
+        {
+        name:"Banana",
+        score:9,
+        review:"I love it"
+        }
+    ], function(err, result) {
+      assert.equal(err, null);
+      assert.equal(3, result.result.n);
+      assert.equal(3, result.ops.length);
+      console.log("Inserted 3 documents into the collection");
+      callback(result);
+    });
+}
+
+
+
+
+// { useUnifiedTopology: true } 
+
+// const mongoose = require('mongoose');
+
+
+// mongoose.connect('mongodb://localhost:27017/Tododb', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// mongoose.connect("mongodb://localhost:27017/fruitsDB", {useNewUrlParser:true});
+
+// const fruitSchema = new mongoose.Schema({
+//     name: String,
+//     rating: Number,
+//     review: String
+// });
+// const Fruit = mongoose.model("fruit", fruitSchema);
+
+// const fruit = new Fruit({
+//     name:"Apple",
+//     rating:5,
+//     review:"Very solid as a fruit."
+// });
+
+// fruit.save();
+
