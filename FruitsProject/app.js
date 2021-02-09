dd9451b0b46ce5d571738e555ae7d50c9e9410fa
luanacesar <luana_cesar@hotmail.com>
2021-02-09 17:57:10 -0500

    add fruits

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -18,9 +18,50 @@ const fruit = new Fruit({
     review:"Very solid as a fruit."
 });
 
+const kiwi = new Fruit({
+  name:"Kiwi",
+  rating:7,
+  review:"good"
+})
+const orange = new Fruit({
+  name:"orange",
+  rating:10,
+  review:"The best fruit"
+})
+const banana = new Fruit({
+  name:"banana",
+  rating:9,
+  review:"My second favorite one"
+})
+
+// Fruit.insertMany([kiwi,orange, banana], function(err){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log("Succesfully saved all the fruits");
+//   }
+// });
 // fruit.save();
 
-const personSchema = new mongoose.Schema({
+Fruit.find(function(err, fruits){
+  if (err){
+    console.log(err);
+  }
+  else{
+    mongoose.connection.close();
+    fruits.forEach(fruit => {
+      console.log(fruit.name);
+    });
+  // fruits.forEach(function (fruit){
+  //   console.log(fruit.name);
+  // });  
+  }
+});
+
+
+
+
+  const personSchema = new mongoose.Schema({
   name: String,
   age: Number,
 });
@@ -34,8 +75,6 @@ const person = new Person({
 
 person.save();
 
-
-
 const findDocuments = function(db, callback) {
   // Get the documents collection
   const collection = db.collection('fruits');
@@ -48,30 +87,4 @@ const findDocuments = function(db, callback) {
   });
 }
 
-
-
-
-// { useUnifiedTopology: true } 
-
-// const mongoose = require('mongoose');
-
-
-// mongoose.connect('mongodb://localhost:27017/Tododb', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// mongoose.connect("mongodb://localhost:27017/fruitsDB", {useNewUrlParser:true});
-
-// const fruitSchema = new mongoose.Schema({
-//     name: String,
-//     rating: Number,
-//     review: String
-// });
-// const Fruit = mongoose.model("fruit", fruitSchema);
-
-// const fruit = new Fruit({
-//     name:"Apple",
-//     rating:5,
-//     review:"Very solid as a fruit."
-// });
-
-// fruit.save();
 
